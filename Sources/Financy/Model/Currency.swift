a983be82d87ad5416e3227270d42579cbbd8bb97
ipasxa <ipasxa@gmail.com>
2022-05-20 21:12:42 +0300

    initial commit

---
+++ Sources/Financy/Model/Currency.swift
@@ -0,0 +1,10 @@
+//
+//  Currency.swift
+//  
+//
+//  Created by ipasxa on 20.05.2022.
+//
+
+public enum Currency {
+    case RUB, USD, EUR
+}
---
+++ Tests/FinancyTests/CurrencyTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import Financy
+
+final class CurrencyTests: XCTestCase {
+    func testExample() throws {
+        // TODO
+    }
+}
